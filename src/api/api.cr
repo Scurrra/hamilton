a5d5cc3d433814def50e50c925c37a378b1e56a4
Scurrra <iscurrra@gmail.com>
2025-08-30 21:27:43 +0300

    API methods generation test

--- src/api/api.cr
+++ src/api/api.cr
@@ -5,12 +5,27 @@ require "./*"
 # Class that is used for communication with Bot API endpoints. 
 class Hamilton::Api
 
+  # :nodoc:
   getter env : Symbol
+
+  # :nodoc:
   getter token : String
+
+  # :nodoc:
   getter url : String
+
+  # :nodoc:
   getter path : String
+
+  # :nodoc:
   getter client : HTTP::Client
 
+  # API class constructor.
+  #
+  # Params:
+  # - `token` -- Bot unique token.
+  # - `url` -- Bot API URL. Default is "https://api.telegram.org", pass your URL if you are using your own server.
+  # - `env` -- Type of the environment. `:test` is used for testing and inserts `test/` in the endpoint after bot token.
   def initialize(@token, @url = "https://api.telegram.org", @env = :prod)
     @path = @env == :test ? "/bot#{@token}/test/" : "/bot#{@token}/" 
     @client = HTTP::Client.new @url
@@ -22,8 +37,8 @@ class Hamilton::Api
     #
     {% end %}   {% if info[:params].size > 0 %} # Params:
     #
-    {% for param, pinfo in info[:params] %} # > `{{param.id}} : {{pinfo[:type]}}`
-    # >
+    {% for param, pinfo in info[:params] %} # `{{param.id}} : {{pinfo[:type]}}`
+    #
     {% for doc, index in pinfo[:docs] %} # > {{doc.id}}
     # >
     {% end %}   #
