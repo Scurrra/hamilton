3268ec3df134307d9cafedeb4d61bfae004c4c8f
Scurrra <iscurrra@gmail.com>
2025-08-30 23:43:10 +0300

    Optional filename for input files

--- src/types/input_file.cr
+++ src/types/input_file.cr
@@ -23,4 +23,8 @@ class Hamilton::Types::InputFile
   # File to be sent.
   @[JSON::Field(ignore: true)]
   property file : File | Nil
+
+  # Filename.
+  @[JSON::Field(ignore: true)]
+  property filename : File | Nil
 end
