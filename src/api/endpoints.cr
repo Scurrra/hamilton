76e09e82bbad3e96e6ab127e15b8c519ca4356cd
Scurrra <iscurrra@gmail.com>
2025-09-02 11:05:55 +0300

    More methods

--- src/api/endpoints.cr
+++ src/api/endpoints.cr
@@ -148,6 +148,190 @@ class Hamilton::Api
           docs: [%<Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard, instructions to remove a reply keyboard or to force a reply from the user.>]
         }
       }
+    },
+    "forwardMessage" => {
+      type: Hamilton::Types::Message,
+      docs: [%<Use this method to forward messages of any kind. Service messages and messages with protected content can't be forwarded. On success, the sent Message is returned.>],
+      parameter: {
+        :chat_id => {
+          type: Int32 | String,
+          docs: [%<Unique identifier for the target chat or username of the target channel (in the format `@channelusername`).>]
+        },
+        :message_thread_id => {
+          type: Int32 | Nil,
+          docs: [%<Unique identifier for the target message thread (topic) of the forum; for forum supergroups only.>]
+        },
+        :direct_messages_topic_id => {
+          type: Int32 | Nil,
+          docs: [%<Identifier of the direct messages topic to which the message will be forwarded; required if the message is forwarded to a direct messages chat.>]
+        },
+        :from_chat_id => {
+          type: Int32 | String,
+          docs: [%<Unique identifier for the chat where the original message was sent (or channel username in the format `@channelusername`).>]
+        },
+        :video_start_timestamp => {
+          type: Int32 | Nil,
+          docs: [%<New start timestamp for the forwarded video in the message.>]
+        },
+        :disable_notification => {
+          type: Bool | Nil,
+          docs: [%<Sends the message silently. Users will receive a notification with no sound.>]
+        },
+        :protect_content => {
+          type: Bool | Nil,
+          docs: [%<Protects the contents of the forwarded message from forwarding and saving.>]
+        },
+        :suggested_post_parameters => {
+          type: Hamilton::Types::SuggestedPostParameters | Nil,
+          docs: [%<A JSON-serialized object containing the parameters of the suggested post to send; for direct messages chats only.>]
+        },
+        :message_id => {
+          type: Int32,
+          docs: [%<Message identifier in the chat specified in `from_chat_id`.>]
+        }
+      }
+    },
+    "forwardMessages" => {
+      type: Array(Hamilton::Types::MessageId),
+      docs: [%<Use this method to forward multiple messages of any kind. If some of the specified messages can't be found or forwarded, they are skipped. Service messages and messages with protected content can't be forwarded. Album grouping is kept for forwarded messages. On success, an array of `MessageId` of the sent messages is returned.>],
+      parameter: {
+        :chat_id => {
+          type: Int32 | String,
+          docs: [%<Unique identifier for the target chat or username of the target channel (in the format `@channelusername`).>]
+        },
+        :message_thread_id => {
+          type: Int32 | Nil,
+          docs: [%<Unique identifier for the target message thread (topic) of the forum; for forum supergroups only.>]
+        },
+        :direct_messages_topic_id => {
+          type: Int32 | Nil,
+          docs: [%<Identifier of the direct messages topic to which the messages will be forwarded; required if the messages are forwarded to a direct messages chat.>]
+        },
+        :from_chat_id => {
+          type: Int32 | String,
+          docs: [%<Unique identifier for the chat where the original messages were sent (or channel username in the format `@channelusername`).>]
+        },
+        :message_ids => {
+          type: Array(Int32),
+          docs: [%<A JSON-serialized list of 1-100 identifiers of messages in the chat `from_chat_id` to forward. The identifiers must be specified in a strictly increasing order.>]
+        },
+        :disable_notification => {
+          type: Bool | Nil,
+          docs: [%<Sends the messages silently. Users will receive a notification with no sound.>]
+        },
+        :protect_content => {
+          type: Bool | Nil,
+          docs: [%<Protects the contents of the forwarded messages from forwarding and saving.>]
+        }
+      }
+    },
+    "copyMessage" => {
+      type: Hamilton::Types::MessageId,
+      docs: [%<Use this method to copy messages of any kind. Service messages, paid media messages, giveaway messages, giveaway winners messages, and invoice messages can't be copied. A quiz poll can be copied only if the value of the field `correct_option_id` is known to the bot. The method is analogous to the method `forwardMessage`, but the copied message doesn't have a link to the original message. Returns the `MessageId` of the sent message on success.>],
+      parameter: {
+        :chat_id => {
+          type: Int32 | String,
+          docs: [%<Unique identifier for the target chat or username of the target channel (in the format `@channelusername`).>]
+        },
+        :message_thread_id => {
+          type: Int32 | Nil,
+          docs: [%<Unique identifier for the target message thread (topic) of the forum; for forum supergroups only.>]
+        },
+        :direct_messages_topic_id => {
+          type: Int32 | Nil,
+          docs: [%<Identifier of the direct messages topic to which the message will be sent; required if the message is sent to a direct messages chat.>]
+        },
+        :from_chat_id => {
+          type: Int32 | String,
+          docs: [%<Unique identifier for the chat where the original message was sent (or channel username in the format `@channelusername`).>]
+        },
+        :message_id => {
+          type: Int32,
+          docs: [%<Message identifier in the chat specified in `from_chat_id`.>]
+        },
+        :video_start_timestamp => {
+          type: Int32 | Nil,
+          docs: [%<New start timestamp for the copied video in the message.>]
+        },
+        :caption => {
+          type: String | Nil,
+          docs: [%<New caption for media, 0-1024 characters after entities parsing. If not specified, the original caption is kept.>]
+        },
+        :parse_mode => {
+          type: String | Nil,
+          docs: [%<Mode for parsing entities in the new caption.>]
+        },
+        :caption_entities => {
+          type: Array(Hamilton::Types::MessageEntity) | Nil,
+          docs: [%<A JSON-serialized list of special entities that appear in the new caption, which can be specified instead of `parse_mode`.>]
+        },
+        :show_caption_above_media => {
+          type: Bool | Nil,
+          docs: [%<Pass True, if the caption must be shown above the message media. Ignored if a new caption isn't specified.>]
+        },
+        :disable_notification => {
+          type: Bool | Nil,
+          docs: [%<Sends the message silently. Users will receive a notification with no sound.>]
+        },
+        :protect_content => {
+          type: Bool | Nil,
+          docs: [%<Protects the contents of the sent message from forwarding and saving.>]
+        },
+        :allow_paid_broadcast => {
+          type: Bool | Nil,
+          docs: [%<Pass True to allow up to 1000 messages per second, ignoring broadcasting limits for a fee of 0.1 Telegram Stars per message. The relevant Stars will be withdrawn from the bot's balance.>]
+        },
+        :suggested_post_parameters => {
+          type: Hamilton::Types::SuggestedPostParameters | Nil,
+          docs: [%<A JSON-serialized object containing the parameters of the suggested post to send; for direct messages chats only. If the message is sent as a reply to another suggested post, then that suggested post is automatically declined.>]
+        },
+        :reply_parameters => {
+          type: Hamilton::Types::ReplyParameters | Nil,
+          docs: [%<Description of the message to reply to.>]
+        },
+        :reply_markup => {
+          type: Hamilton::Types::InlineKeyboardMarkup | Hamilton::Types::ReplyKeyboardMarkup | Hamilton::Types::ReplyKeyboardRemove | Hamilton::Types::ForceReply | Nil,
+          docs: [%<Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard, instructions to remove a reply keyboard or to force a reply from the user.>]
+        }
+      }
+    },
+    "copyMessages" => {
+      type: Array(Hamilton::Types::MessageId),
+      docs: [%<Use this method to copy messages of any kind. If some of the specified messages can't be found or copied, they are skipped. Service messages, paid media messages, giveaway messages, giveaway winners messages, and invoice messages can't be copied. A quiz poll can be copied only if the value of the field `correct_option_id` is known to the bot. The method is analogous to the method `forwardMessages`, but the copied messages don't have a link to the original message. Album grouping is kept for copied messages. On success, an array of `MessageId` of the sent messages is returned.>],
+      parameter: {
+        :chat_id => {
+          type: Int32 | String,
+          docs: [%<Unique identifier for the target chat or username of the target channel (in the format `@channelusername`).>]
+        },
+        :message_thread_id => {
+          type: Int32 | Nil,
+          docs: [%<Unique identifier for the target message thread (topic) of the forum; for forum supergroups only.>]
+        },
+        :direct_messages_topic_id => {
+          type: Int32 | Nil,
+          docs: [%<Identifier of the direct messages topic to which the messages will be sent; required if the messages are sent to a direct messages chat.>]
+        },
+        :from_chat_id => {
+          type: Int32 | String,
+          docs: [%<Unique identifier for the chat where the original messages were sent (or channel username in the format `@channelusername`).>]
+        },
+        :message_ids => {
+          type: Array(Int32),
+          docs: [%<A JSON-serialized list of 1-100 identifiers of messages in the chat `from_chat_id` to copy. The identifiers must be specified in a strictly increasing order.>]
+        },
+        :disable_notification => {
+          type: Bool | Nil,
+          docs: [%<Sends the messages silently. Users will receive a notification with no sound.>]
+        },
+        :protect_content => {
+          type: Bool | Nil,
+          docs: [%<Protects the contents of the sent messages from forwarding and saving.>]
+        },
+        :remove_caption => {
+          type: Bool | Nil,
+          docs: [%<Pass True to copy the messages without their captions.>]
+        }
+      }
     }
   }
 end
