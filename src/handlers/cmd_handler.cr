15cc0defdf644a726d8a365a429db9640ded7bad
Scurrra <iscurrra@gmail.com>
2025-10-26 01:28:48 +0300

    CmdHandler for some types of payload

--- src/handlers/cmd_handler.cr
+++ src/handlers/cmd_handler.cr
@@ -10,6 +10,8 @@ end
 
 module Hamilton::CmdHandler
   include Hamilton::Handler
+
+  PAYLOAD_TYPES = [:animation, :audio, :document, :paid_media, :photo, :sticker, :story, :video, :video_note, :voice, :checklist, :contact, :contact, :dice, :game, :poll, :venue, :location, :invoice, :successful_payment, :refunded_payment, :users_shared, :chat_shared, :passport_data]
   
   property mapper : Hash(Symbol, Hash(String | Symbol, Symbol))
   property context : Hamilton::Context
@@ -17,24 +19,289 @@ module Hamilton::CmdHandler
   property log : Log
 
   def initialize
-    @log = Log.for("Hamilton::Bot")
+    @log = Log.for("Hamilton::Bot Command Handler")
     @context = Hamilton::Context.new default_method: :root
     @mapper = {:root => Hash(String | Symbol, Symbol).new}
   end
 
-  # TODO: override call
+  def call(update : Hamilton::Types::Update)
+    update_types = update.non_nil_fields
+    update_types.delete("update_id")
+    if update_types.size == 0
+      @log.error { "Empty update" }
+    elsif update_types[0] == "message" || update_types[0] == "business_message"
+      # message itself
+      message = update.message
+      message ||= update.business_message
+
+      # as we have message we definitely have non-nil context 
+      # for empty context it will be created when calling `get_method` with `{method: :root, data: nil}` value
+      ctxt_method = @context.get_method(message.chat.id)
+
+      # possible messages mapper for the last method from the chat's context
+      # should not be nil
+      pmm = @mapper[ctxt_method]
+      
+      # non-nil fields
+      message_fields = message.non_nil_fields
+
+      # first check for command or known text payload from `ReplyKeyboard`
+      case message_fields
+      when .includes?("text")
+        #TODO: known texts and commands
+      
+      #TODO: replace following with macro generator
+      when .includes?("animation")
+        if method = pmm[:animation]?
+          {{method.id}}.call(animation: message.animation, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:animation` payload that can not be processed" }
+        end
+      when .includes?("audio")
+        if method = pmm[:audio]?
+          {{method.id}}.call(audio: message.audio, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:audio` payload that can not be processed" }
+        end
+      when .includes?("document")
+        if method = pmm[:document]?
+          {{method.id}}.call(document: message.document, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:document` payload that can not be processed" }
+        end
+      when .includes?("paid_media")
+        if method = pmm[:paid_media]?
+          {{method.id}}.call(paid_media: message.paid_media, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:paid_media` payload that can not be processed" }
+        end
+      when .includes?("photo")
+        if method = pmm[:photo]?
+          {{method.id}}.call(photo: message.photo, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:photo` payload that can not be processed" }
+        end
+      when .includes?("sticker")
+        if method = pmm[:sticker]?
+          {{method.id}}.call(sticker: message.sticker, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:sticker` payload that can not be processed" }
+        end
+      when .includes?("story")
+        if method = pmm[:story]?
+          {{method.id}}.call(story: message.story, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:story` payload that can not be processed" }
+        end
+      when .includes?("video")
+        if method = pmm[:video]?
+          {{method.id}}.call(video: message.video, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:video` payload that can not be processed" }
+        end
+      when .includes?("video_note")
+        if method = pmm[:video_note]?
+          {{method.id}}.call(video_note: message.video_note, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:video_note` payload that can not be processed" }
+        end
+      when .includes?("voice")
+        if method = pmm[:voice]?
+          {{method.id}}.call(voice: message.voice, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:voice` payload that can not be processed" }
+        end
+      when .includes?("checklist")
+        if method = pmm[:checklist]?
+          {{method.id}}.call(checklist: message.checklist, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:checklist` payload that can not be processed" }
+        end
+      when .includes?("contact")
+        if method = pmm[:contact]?
+          {{method.id}}.call(contact: message.contact, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:contact` payload that can not be processed" }
+        end
+      when .includes?("dice")
+        if method = pmm[:dice]?
+          {{method.id}}.call(dice: message.dice, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:dice` payload that can not be processed" }
+        end
+      when .includes?("game")
+        if method = pmm[:game]?
+          {{method.id}}.call(game: message.game, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:game` payload that can not be processed" }
+        end
+      when .includes?("poll")
+        if method = pmm[:poll]?
+          {{method.id}}.call(poll: message.poll, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:poll` payload that can not be processed" }
+        end
+      when .includes?("venue")
+        if method = pmm[:venue]?
+          {{method.id}}.call(venue: message.venue, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:venue` payload that can not be processed" }
+        end
+      when .includes?("location")
+        if method = pmm[:location]?
+          {{method.id}}.call(location: message.location, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:location` payload that can not be processed" }
+        end
+      when .includes?("invoice")
+        if method = pmm[:invoice]?
+          {{method.id}}.call(invoice: message.invoice, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:invoice` payload that can not be processed" }
+        end
+      when .includes?("successful_payment")
+        if method = pmm[:successful_payment]?
+          {{method.id}}.call(successful_payment: message.successful_payment, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:successful_payment` payload that can not be processed" }
+        end
+      when .includes?("refunded_payment")
+        if method = pmm[:refunded_payment]?
+          {{method.id}}.call(refunded_payment: message.refunded_payment, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:refunded_payment` payload that can not be processed" }
+        end
+      when .includes?("users_shared")
+        if method = pmm[:users_shared]?
+          {{method.id}}.call(users_shared: message.users_shared, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:users_shared` payload that can not be processed" }
+        end
+      when .includes?("chat_shared")
+        if method = pmm[:chat_shared]?
+          {{method.id}}.call(chat_shared: message.chat_shared, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:chat_shared` payload that can not be processed" }
+        end
+      when .includes?("passport_data")
+        if method = pmm[:passport_data]?
+          {{method.id}}.call(passport_data: message.passport_data, context_key: message.chat.id, update: update)
+        else
+          @log.warn { "Update #{update.update_id} is of type `message`/`business_message` and contains `:passport_data` payload that can not be processed" }
+        end
+      else
+        @log.info { "Update #{update.update_id} is of type `message`/`business_message` and contains payload that can not be processed by Command Handler" }
+      end
+
+    elsif update_types[0] == "callback_query"
+      @log.warn { "Hamilton's developer doesn't recommend to use `InlineKeybord`s" }
+      
+      update_data = update.callback_query.data
+      update_data ||= update.callback_query.game_short_name
+      if data = update_data 
+        # there are three options how to identify chat or user who sent this
+        
+        # 1. `chat_instance` --- "Global identifier, uniquely corresponding to the chat to which the message with the callback button was sent."
+        # This is the only type where `chat_instance` is used, but it always presents here.
+        if chat_instance = update.callback_query.chat_instance
+          # one more erason to not use this shit: "Data associated with the callback button. Be aware that the message originated the query can contain no callback buttons with this data." 
+          # as `chat_instance` is never null, the `get_method?` method is used to not get `:root` 
+          # P.S. I didn't understand what `chat_instance` is so here is some workaround to not fuck up
+          if ctxt_method = @context.get_method?(chat_instance)
+            if @mapper.has_key?(ctxt_method)
+              if method = @mapper[ctxt_method][data]?
+                {{method.id}}.call(callback: data, context_key: chat_instance, update: update)
+                return call_next(update)
+              else
+                @log.warn { "Update #{update.update_id} is of type `callback_query` and doesn't have a handler for the provided payload when handled with `chat_instance`" }
+              end
+            end
+          end
+        end
+        
+        # 2. usual and most normal option: detect chat the original message (that one with `inline_keyboard`) was sent
+        # `callback_query` may have `message` field from which we may try to extract the chat's id
+        if message = update.callback_query.message
+          # message is of type `Hamilton::Types::MaybeInaccessibleMessage` and anyways contains chat info
+          chat_id = message.chat.id
+          # let's assume that `callback_query` can not be sent in the chat with empty context
+          # i.e. to `:root`
+          if ctxt_method = @context.get_method?(chat_id)
+            if @mapper.has_key?(ctxt_method)
+              if method = @mapper[ctxt_method][data]?
+                {{method.id}}.call(callback: data, context_key: chat_id, update: update)
+                return call_next(update)
+              else
+                @log.warn { "Update #{update.update_id} is of type `callback_query` and doesn't have a handler for the provided payload when handled with `message.chat.id`" }
+              end
+            end
+          end
+        end
+
+        # 3. for some reason let's check sender's id
+        # `callback_query` contains `from` field from which sender's id may be obtained
+        if user_id = update.callback_query.from.id
+          # if the only way to handle `callback_query` is with user id, there is probably no known context
+          # in this case ctxt_method = ctxt[:method] will be ":root" and data will be empty
+          # `get_method?` is called to not create new context in case of error
+          ctxt_method = @context.get_method?(user_id) || :root
+          if @mapper.has_key?(ctxt_method)
+            if method = @mapper[ctxt_method][data]?
+              {{method.id}}.call(callback: data, context_key: user_id, update: update)
+              return call_next(update)
+            else
+              @log.warn { "Update #{update.update_id} is of type `callback_query` and doesn't have a handler for the provided payload when handled with `from.id` (sender's/user id)" }
+            end
+          end
+        end
+      
+      else
+        @log.warn { "Update #{update.update_id} is of type `callback_query` and doesn't contain any payload. If the update won't be procesed user may hang (see the note for `Hamilton::Types::CallbackQuery`)" }
+      end
+    else
+      @log.info { "Update #{update.update_id} was skipped by `CmdHandler` due to unknown update type" }
+    end
+
+    call_next(update)
+  end
 
   macro method_added(method)
     {% if method.annotation(Handle) %}
+      {% unless method.args.map(&.name.symbolize).includes?(:context_key) %}
+        raise MissingCmdHandlerMethodParam.new :context_key
+      {% end %}
+
+      {% unless method.args.map(&.name.symbolize).includes?(:update) %}
+        raise MissingCmdHandlerMethodParam.new :update
+      {% end %}
+      
       {% if method.annotation(Handle).named_args.has_key?(:command) %}
-      %type = :command
       %value = {{method.annotation(Handle)[:command].stringify}}
+      {% unless method.args.map(&.name.symbolize).includes?(:command) %}
+        raise MissingCmdHandlerMethodParam.new :command
+      {% end %}
+      {% elsif method.annotation(Handle).named_args.has_key?(:callback) %}
+      %value = {{method.annotation(Handle)[:callback].stringify}}
+      {% unless method.args.map(&.name.symbolize).includes?(:callback) %}
+        raise MissingCmdHandlerMethodParam.new :callback
+      {% end %}
       {% elsif method.annotation(Handle).named_args.has_key?(:text) %}
-      %type = :text
       %value = {{method.annotation(Handle)[:text].stringify}}
-      {% else %}
-      %type = :other
+      {% unless method.args.map(&.name.symbolize).includes?(:text) %}
+        raise MissingCmdHandlerMethodParam.new :text
+      {% end %}
+      {% elsif method.annotation(Handle).args.size != 0 %}
       %value = {{method.annotation(Handle)[0].symbolize}}
+
+      {% unless PAYLOAD_TYPES.includes?(method.annotation(Handle)[0].symbolize) %}
+        raise UnspportedCmdHandlerPayloadType.new %value
+      {% end %}
+
+      {% unless method.args.map(&.name.symbolize).includes?(method.annotation(Handle)[0].symbolize) %}
+        raise MissingCmdHandlerMethodParam.new %value
+      {% end %}
+      {% else %}
+        raise MissingCmdHandlerMethodAnnotationArg.new "Handle"
       {% end %}
 
       {% for method_ in method.annotation(For).args %}
