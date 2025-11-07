2299ebd00ea19d6d2f91718ecb5e16a053a93a75
Scurrra <iscurrra@gmail.com>
2025-11-08 02:13:20 +0300

    Proper creation of Hamilton::Types:: with .new without JSON

--- src/types/utils.cr
+++ src/types/utils.cr
@@ -1,33 +1,36 @@
 # Module that encapsulates type construction and serialization logic (initialize from `**params` and `from_json`; `to_json`).
 module Hamilton::Types::Common
   
+  macro included
+
   def self.new(**params)
-    instance = {{@type.name}}.allocate
-    {% for var, index in @type.instance_vars %} 
-      {% unless var.name.stringify == "non_nil_fields" %}
-        {% if Nil <= var.type %}
-        instance.{{var.name}} = params[{{var.name.stringify}}]?
-        {% else %}
-        instance.{{var.name}} = params[{{var.name.stringify}}]
-        {% end %}
-      {% end %}   
-    {% end %}
-    
-    instance.after_initialize
+    instance = allocate
+    instance.initialize(params)
     instance
   end
 
-  # def initialize
-  #   {% for var, index in @type.instance_vars %} {% unless var.name.stringify == "non_nil_fields" %}
-  #   @{{var.name}}, {% end %}
-  #   {% end %}
-  
-  #   after_initialize
-  # end
+  def initialize(params)
+    {% verbatim do %}
+    {% for var, index in @type.instance_vars %} 
+      {% unless var.name.stringify == "non_nil_fields" %}
+        {% if Nil <= var.type %}
+        @{{var.name}} = params[{{var.name.stringify}}]?
+        {% else %}
+        @{{var.name}} = params[{{var.name.stringify}}]
+        {% end %}
+
+        unless @{{var.name}}.nil?
+          @non_nil_fields.push({{var.name.stringify}})
+        end
+      {% end %}   
+    {% end %}
+    {% end %}
+  end
 
   # List of available non-nil fields.
   @[JSON::Field(ignore: true)]
   property non_nil_fields : Array(String) = [] of String
+  end
 
   # :nodoc:
   def after_initialize
